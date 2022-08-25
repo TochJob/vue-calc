43feda3f9d1eb92d4cbceed13dc67eaa50e557ee
Toch <tochjob@gmail.com>
2022-08-25 11:40:24 +0300

    calc

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,74 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="number" v-model.number="num1">
+    <input type="number" v-model.number="num2">
+    <button @click="mth('+')">+</button>
+    <button @click="mth('-')">-</button>
+    <button @click="mth('*')">*</button>
+    <button @click="mth('/')">/</button>
+    <button @click="mth('^')">^</button>
+    <button @click="mth('%')">%</button>
+    {{ res }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+
+  },
+  data() {
+    return {
+      num1:0,
+      num2:0,
+      res:0
+    }
+  },
+  methods: {
+    sum () {
+      this.res=this.num1+this.num2
+    },
+    minus () {
+      this.res=this.num1-this.num2
+    },
+    div () {
+      this.res=this.num1/this.num2
+    },
+    mult () {
+      this.res=this.num1*this.num2
+    },
+    exponentiation () {
+      this.res=Math.pow(this.num1, this.num2)
+    },
+    integer () {
+      this.res=Math.trunc(this.num1 / this.num2)
+    },
+    mth (key) {
+      switch (key) {
+        case '+':
+          this.sum()
+          break;
+        case '-':
+          this.minus()
+          break;
+        case '*':
+          this.mult()
+          break;
+        case '/':
+          this.div()
+          break;
+        case '%':
+          this.integer()
+          break;
+        case '^':
+          this.exponentiation()
+          break;
+      }
+    }
+  },
 }
 </script>
 
